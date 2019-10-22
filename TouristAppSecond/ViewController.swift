ff2d50ed3471e429eb93ccc0008443c11088f691
Tomasz Iwaszek <tiwaszek@pgs-soft.com>
2019-10-22 15:14:42 +0200

    Initial Commit

---
+++ TouristAppSecond/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouristAppSecond
+//
+//  Created by TIWASZEK on 22/10/2019.
+//  Copyright © 2019 TIWASZEK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
